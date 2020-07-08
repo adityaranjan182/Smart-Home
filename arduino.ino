c7ac96828efe2ba0b468984e365a0d416e243e5e
Aditya Ranjan <57028840+adityaranjan182@users.noreply.github.com>
2020-07-09 01:32:16 +0530

    updated

--- arduino.ino
+++ arduino.ino
@@ -1,56 +1,90 @@
-#include <ArduinoJson.h>
+#include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
-#include <FirebaseArduino.h>
-
 
 #define FIREBASE_HOST "smart-home-e7748.firebaseio.com"
 #define FIREBASE_AUTH "K6vgj05vG7KJQvVujNaCxacTz5ytyUKsFGjfP6XI"
 #define WIFI_SSID "realme X"
 #define WIFI_PASSWORD "01234567"
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(D2, OUTPUT);
+FirebaseData firebaseData1;
+FirebaseData firebaseData2;
 
-  // connect to wifi.
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("connecting");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.print("connected: ");
-  Serial.println(WiFi.localIP());
-  
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-      Firebase.set("led_status", 0);
+void setup()
+{
+
+    Serial.begin(115200);
+    pinMode(5, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(0, OUTPUT);
+    pinMode(2, OUTPUT);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
+
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+    //Enable auto reconnect the WiFi when connection lost
+    Firebase.reconnectWiFi(true);
+    if (Firebase.setInt(firebaseData1, "/bulb_status", 0))
+    {
+        //Success
+        Serial.println("Set int data success");
+    }
+    else
+    {
+        //Failed?, get the error reason from firebaseData
+
+        Serial.print("Error in setInt, ");
+        Serial.println(firebaseData1.errorReason());
+    }
+    if (Firebase.setInt(firebaseData2, "/fan_status", 0))
+    {
+        //Success
+        Serial.println("Set int data success");
+    }
+    else
+    {
+        //Failed?, get the error reason from firebaseData
+
+        Serial.print("Error in setInt, ");
+        Serial.println(firebaseData2.errorReason());
+    }
 }
 
-int n = 0;
-
-void loop() {
-  // set value
-  // handle error
-  if (Firebase.failed()) {
-      Serial.print("setting /number failed:");
-      Serial.println(Firebase.error());  
-      return;
-  }
-  delay(500);
-  
-  // update value
-  n = Firebase.getInt("led_status");
-  // handle error
-  if (n==1) {
-      Serial.println("led get turned on");
-      digitalWrite(D2, HIGH); 
-      delay(500); 
-      return;
-  }else{
-      Serial.println("led get turned off");
-      digitalWrite(D2, LOW); 
-      delay(500);
-      return; 
-  }
+void loop()
+{
+    int bulb = firebaseData1.intData();
+    int fan = firebaseData2.intData();
+    if (Firebase.getInt(firebaseData1, "bulb_status"))
+    {
+        if (bulb == 1)
+        {
+            Serial.println("bulb on");
+        }
+        else
+        {
+            Serial.println("bulb off");
+        }
+    }
+    if (Firebase.getInt(firebaseData2, "fan_status"))
+    {
+        if (fan == 1)
+        {
+            Serial.println("fan on");
+        }
+        else
+        {
+            Serial.println("fan off");
+        }
+    }
+    delay(500);
 }
